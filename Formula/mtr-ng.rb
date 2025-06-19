10ed2859a9c46b735c1bd93e628be1e3d3a3a425
Edwin de Jong <112636081+edejong-dbc@users.noreply.github.com>
2025-06-19 21:33:36 +0200

    mtr-ng 0.1.5
    Created by https://github.com/edejong-dbc/mtr-ng/actions/runs/15765074574

--- Formula/mtr-ng.rb
+++ Formula/mtr-ng.rb
@@ -1,6 +1,6 @@
 class MtrNg < Formula
   desc "Modern My Traceroute with real-time network path visualization"
   homepage "https://github.com/edejong-dbc/mtr-ng"
-  url "https://github.com/edejong-dbc/mtr-ng/archive/v0.1.4.tar.gz"
+  url "https://github.com/edejong-dbc/mtr-ng/archive/v0.1.5.tar.gz"
   license "MIT OR Apache-2.0"
 end
